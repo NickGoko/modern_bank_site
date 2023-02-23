4838cbfdaa2534d3cba48aba3086ae591d50cbb0
NickGoko <nickmainagoko@gmail.com>
2023-02-23 22:36:36 +0300

    first-commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import NavBar from './NavBar';
+import Hero from './Hero';
+import Button from './Button';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import FeedBackCard from './FeedBackCard';             
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+import Business from './Business';
+import Billing from './Billing';
+
+
+export{
+    NavBar,
+    Hero,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedBackCard,
+    Footer,
+    GetStarted,
+    Stats,
+    Testimonials,
+    Billing,
+    Business
+
+}
