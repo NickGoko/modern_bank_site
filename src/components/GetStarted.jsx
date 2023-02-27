7132ecc6e2d2044d147f3583f885f3e32511794c
NickGoko <nickmainagoko@gmail.com>
2023-02-27 14:56:33 +0300

    adding comments to GetStarted

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,8 +3,12 @@ import { arrowUp } from "../assets";
 
 const GetStarted = () => (
   <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-    <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
-      <div className={`${styles.flexStart} flex-row`}>
+    <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+    // sets Get, arrow image and Started on flex-col
+    >
+      <div className={`${styles.flexStart} flex-row`}
+      // sets Get and arrow on the flex-row putting them on the same row
+      >
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
           <span className="text-gradient">Get</span>
         </p>
