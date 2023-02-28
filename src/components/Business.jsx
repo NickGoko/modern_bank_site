9e18f5e2d9392e7512475a87c8b734b2fde378f7
NickGoko <nickmainagoko@gmail.com>
2023-02-28 14:19:22 +0300

    adding comments

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,12 +3,16 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 const FeatureCard = ({ icon, title, content, index }) => (
-  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
-    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+  
+  // Set up a functional react component of FeatureCard that defines the markup and behavior of a single feature 
+  // that is used in Business component
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}
+  // if index not equal not the last index margin-bottom is 6 else last index has margin-bottom of 0
+  ><div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
     </div>
     <div className="flex-1 flex flex-col ml-3">
-      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-3">
         {title}
       </h4>
       <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
