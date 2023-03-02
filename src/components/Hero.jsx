e3fd513a001273fffc30a554244061f3476d1961
NickGoko <nickmainagoko@gmail.com>
2023-03-02 12:52:31 +0300

    adding comments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
         <img src={hero1} alt="hero" className="w-[60%] h-[90%] relative z-[20] overflow-hidden rounded-sm mx-5" />
 
         {/* gradient start */}
-        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 orange__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
         {/* gradient end */}
