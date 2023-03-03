2182003eda4ced246b72105559563445b3166354
NickGoko <nickmainagoko@gmail.com>
2023-03-03 12:21:30 +0300

    commenting out the img files

--- src/constants/index.js
+++ src/constants/index.js
@@ -50,7 +50,7 @@ export const feedback = [
       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
     name: "Herman Jensen",
     title: "Founder & Leader",
-    img: people01,
+    // img: people01,
   },
   {
     id: "feedback-2",
@@ -58,7 +58,7 @@ export const feedback = [
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
     name: "Steve Mark",
     title: "Founder & Leader",
-    img: people02,
+    // img: people02,
   },
   {
     id: "feedback-3",
@@ -66,7 +66,15 @@ export const feedback = [
       "It is usually people in the money business, finance, and international trade that are really rich.",
     name: "Kenn Gallagher",
     title: "Founder & Leader",
-    img: people03,
+    // img: people03,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    // img: people03,
   },
 ];
 
