471fcc243c592156b34ff23c8f9d724ae4699dbf
NickGoko <nickmainagoko@gmail.com>
2023-03-03 12:12:14 +0300

    adding one line comment

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -8,7 +8,7 @@ const CardDeal = () => (
       <h2 className={styles.heading2}>
         Find a better card deal <br className="sm:block hidden " /> in few easy
         steps.
-        {/*Using sm:block hidden means the br element is hidden  */}
+        {/*Using sm:block will be block display in screens at least 640px hidden means the br element is hidden in smaller screens than sm(min-width-640)  */}
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
       Save money on your credit card with our easy-to-use tools.
