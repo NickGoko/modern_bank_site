e30f6e7df78bed5154bfbbf9b757c8553818d37c
NickGoko <nickmainagoko@gmail.com>
2023-03-06 11:11:42 +0300

    changing the color gradient

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,7 +2,7 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 
 const GetStarted = () => (
-  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-orange-gradient p-[2px] cursor-pointer`}>
     <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
     // sets Get, arrow image and Started on flex-col
     >
@@ -10,13 +10,13 @@ const GetStarted = () => (
       // sets Get and arrow on the flex-row putting them on the same row
       >
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
-          <span className="text-gradient">Get</span>
+          <span className="text-orange-gradient">Get</span>
         </p>
         <img src={arrowUp} alt="arrow-up" className="w-[33px] h-[23px] object-contain" />
       </div>
       
       <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
-        <span className="text-gradient">Started</span>
+        <span className="text-orange-gradient">Started</span>
       </p>
     </div>
   </div>
